797aea1a9b5856e4a73836fa7d1fab7b4734d5d3
Harsh3305 <harshverma3305@gmail.com>
2021-06-12 19:09:24 +0530

    use const

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
+
+function add() {
+  return (
+    <b1>
+      <h1>Hello $name</h1>
+    </b1>
+  );
+}
+
+const Greeting = (probs) => {
+  console.log(probs);
+
+  return (
+    <h1>
+      Welcome, {probs.name} (your age is {probs.age})
+    </h1>
+  );
+};
 
 function App() {
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       {/* <p>
+  //         Edit <code>src/App.js</code> and save to reload.
+  //       </p>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Learn React
+  //       </a> */}
+  //     </header>
+  //   </div>
+  // );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Greeting name="Alice" age={30}></Greeting>
     </div>
   );
 }
